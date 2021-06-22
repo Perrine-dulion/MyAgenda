2947d05be838e67ff2df6e4c42a43d1dfc2cc84d
Perrine <perrine.dulion@gmail.com>
2021-06-22 14:48:43 +0200

    test

--- app/src/main/java/com/example/myagenda/MainActivity.java
+++ app/src/main/java/com/example/myagenda/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myagenda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvWeather = findViewById(R.id.tv_main_weather);
+
+        RequestWeatherTask requestWeatherTask = new RequestWeatherTask();
+
+        requestWeatherTask.setWeatherListener(data -> {
+            tvWeather.setText(data.getCity() + " " + data.getTemp() + "°c");
+        });
+
+        requestWeatherTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,"Bruxelles, CA");
     }
 }
--- app/src/main/java/com/example/myagenda/models/TodoTask.java
+++ app/src/main/java/com/example/myagenda/models/TodoTask.java
@@ -1,4 +1,72 @@
 package com.example.myagenda.models;
 
+import java.time.LocalDate;
+
 public class TodoTask {
+
+    private long todoTaskId;
+    private String name;
+    private LocalDate creationDate;
+    private LocalDate limitDate;
+    private LocalDate finishDate;
+
+    public TodoTask(long todoTaskId, String name, LocalDate creationDate, LocalDate limitDate, LocalDate finishDate) {
+        this(name,  creationDate, limitDate);
+        this.todoTaskId = todoTaskId;
+        this.finishDate = finishDate;
+    }
+
+    public TodoTask(String name, LocalDate creationDate, LocalDate limitDate) {
+        this.todoTaskId = 0;
+        this.name = name;
+        this.creationDate = creationDate;
+        this.limitDate = limitDate;
+        this.finishDate = null;
+    }
+
+
+    public long getTodoTaskId() {
+        return todoTaskId;
+    }
+
+    public void setTodoTaskId(long todoTaskId) {
+        this.todoTaskId = todoTaskId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDate getLimitDate() {
+        return limitDate;
+    }
+
+    public void setLimitDate(LocalDate limitDate) {
+        this.limitDate = limitDate;
+    }
+
+    public LocalDate getFinishDate() {
+        return finishDate;
+    }
+
+    public void setFinishDate(LocalDate finishDate) {
+        this.finishDate = finishDate;
+    }
+
+    public boolean isDone() {
+        return this.finishDate != null;
+    }
 }
