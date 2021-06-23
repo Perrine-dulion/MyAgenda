4658e9debdf9e3bdfd5a0fc3cd69fc84f3bb41af
Perrine <perrine.dulion@gmail.com>
2021-06-23 17:03:34 +0200

    endOfDayBloquépassageecran1a2

--- app/src/main/java/com/example/myagenda/MainActivity.java
+++ app/src/main/java/com/example/myagenda/MainActivity.java
@@ -23,14 +23,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 //TODO click vers new event //
+        //chregneugneucamarchepas voir cours/ forum//
+
+
+        AddNewEvent addNewEvent = new AddNewEvent();
         RecyclerView lvTasks;
         Button btnadd = (Button) findViewById(R.id.btn_add_task_valid) ;
         btnadd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) { }
 
-            private void openAddNewEvent() {
+            private void openAddNewEvent() {openAddNewEvent();
             }
         });
 
