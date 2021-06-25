3ae93457cab82c41b0fa269d46d81fd66dd51ff4
Perrine <perrine.dulion@gmail.com>
2021-06-25 15:30:53 +0200

    AjoutCalendar

--- app/src/main/java/com/example/myagenda/AddNewEvent.java
+++ app/src/main/java/com/example/myagenda/AddNewEvent.java
@@ -1,13 +1,56 @@
 package com.example.myagenda;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AddNewEvent extends AppCompatActivity {
+public class AddNewEvent extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_new_event);
+
+        Spinner spinner = findViewById(R.id.sp_add_task_category);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.categorie, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        Button selectDate = findViewById(R.id.calendar_pick);
+        TextView date = findViewById(R.id.picked_date);
+
+        selectDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        DatePickerDialog datePickerDialog = new DatePickerDialog(AddNewEvent.this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+
+                    }
+                }, 0, 0, 0);
+
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
